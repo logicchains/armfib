2720f549a497f69632e7feff67ebe839e7051ce4
Huon Wilson <dbau.pp+github@gmail.com>
2014-08-05 20:25:04 +1000

    Use `i32` rather than `int` in the Rust code
    C's `int` is 32-bits, but the Rust's `int` is (currently) pointer-sized, i.e. 64-bits on x86-64. This changes the Rust to using a matching integer type, giving much better performance:
    
        75699
        LANGUAGE Rust 2167
        75699
        LANGUAGE C 2220
        75699
        LANGUAGE C 2271

--- fib.rs
+++ fib.rs
@@ -1,25 +1,25 @@
 extern crate time;
 
-use std::os; 
+use std::os;
 use time::precise_time_ns;
 
-fn fib_rec(qianVal:int,zuoVal:int,rem:int)->int{
+fn fib_rec(qianVal:i32,zuoVal:i32,rem:i32)->i32{
   if rem == 0{
-    zuoVal 
+    zuoVal
   }else{
     fib_rec(zuoVal,qianVal+zuoVal,rem-1)
-  }  
+  }
 }
 
-fn fib(x:int)->int{
+fn fib(x:i32)->i32{
   if x < 2{
     x
   }else{
     fib_rec(0,1,x-1)
-  } 
+  }
 }
 
-fn do_work(acc:int,rem:int)->int{
+fn do_work(acc:i32,rem:i32)->i32{
   if rem == 0 {
     acc
   }else{
@@ -28,9 +28,9 @@ fn do_work(acc:int,rem:int)->int{
 }
 
 fn main() {
-  let n : int = from_str(os::args().get(1).as_slice()).expect("First argument must be an int");
+  let n : i32 = from_str(os::args().get(1).as_slice()).expect("First argument must be an i32");
   let startTime = precise_time_ns();
   println!("{}",do_work(0,n));
   let duration = (precise_time_ns() - startTime) / 1000000;
-  println!("LANGUAGE  Rust  {}", duration);
+  println!("LANGUAGE Rust {}", duration);
 }
